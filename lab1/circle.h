2d8c3f23fbb261e1fec12057aac96a774f5f2ba1
malumbaaa <60159230+malumbaaa@users.noreply.github.com>
2020-06-29 04:19:07 +0300

    Create circle.h

---
+++ lab1/circle.h
@@ -0,0 +1,17 @@
+//---------------------------------------------------------------------------
+
+#ifndef CurcleH
+#define CurcleH
+//---------------------------------------------------------------------------
+#endif
+ #include <vcl.h>
+
+class Curcle
+{
+		  int x1,y1,x2,y2;
+		  public:
+		  void draw(int a,int b,int c,int d,TCanvas *Canvas);
+		  int Square (int a,int b,int c,int d,TCanvas *Canvas);
+		  int Perimetr (int a,int b,int c,int d,TCanvas *Canvas);
+
+} ;
